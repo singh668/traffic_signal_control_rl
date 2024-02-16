c6cf7fcfa731cead76c8f0b76c4ac99055bd887f
Siddharth Singh <siddharthsingh@Siddharths-MBP.lan>
2024-02-16 10:56:04 -0800

    Functions and Error Handling

--- simple_atm.java
+++ simple_atm.java
@@ -20,6 +20,7 @@ public class simple_atm {
         int func_answer;
         try {
             func_answer = sc.nextInt();
+            sc.nextLine();
         } catch (InputMismatchException e ) {
             System.out.println("Improper Response. Please try again.");
             sc.nextLine();
@@ -30,7 +31,38 @@ public class simple_atm {
     }
 
     public static void responseConfirmationIntro(int response) {
-        System.out.println("Arrived here!");
+        String r1 = "You have chosen: ";
+        String r2 = "Is this correct? [Y] or [N]";
+        String conf;
+        switch (response) {
+            case 1:
+                System.out.println(r1 + "Deposit Amount. " + r2);
+                conf = sc.nextLine();
+                taskChoiceConfirmation("Deposit", conf);
+                break;
+            case 2:
+                System.out.println(r1 + "Withdraw Amount. " + r2);
+                conf = sc.nextLine();
+                taskChoiceConfirmation("Withdraw", conf);
+                break;
+            case 3:
+                System.out.println("You have successfully exited the ATM. Please come again.");
+                break;
+            default:
+                System.out.println("Incorrect response. Please try again");
+                introSequence();
+                break;
+        }
+    }
+
+    public static void taskChoiceConfirmation(String task, String confirmation) {
+        if (confirmation.toLowerCase().equals("y")) {
+            System.out.println("You have confirmed your choice of: " + task + ". Lets continue");
+        }
+        else {
+            System.out.println("Please try again");
+            introSequence();
+        }
     }
     public static void main(String[] args) {
         sc = new Scanner(System.in);
