24dadbee3798dc19cbac3d78f999ffe8d0678cf8
Siddharth Singh <siddharthsingh@Siddharths-MBP.lan>
2024-02-16 10:13:07 -0800

    Added code for improper code handling

--- simple_atm.java
+++ simple_atm.java
@@ -1,28 +1,41 @@
 /**
  * Simple Atm
+ * Note: A static method belongs to the class. A non-static method belongs to an instance of the class.
  */
 
  import java.util.Scanner;
+ import java.util.InputMismatchException;
 
 
 public class simple_atm {
+    private static Scanner sc;
     public int total_int = 25000;
-    public static void main(String[] args) {
-        String intro_question = "Welcome to an ATM machine! How can I help you?" + 
-        "1: Deposit Amount" + 
-        "2: Withdraw Amount";
+
+    public static void introSequence() {
+        String intro_question = "How can I help you?\n" + 
+        "1: Deposit Amount\n" + 
+        "2: Withdraw Amount\n" + 
+        "3: Exit";
         System.out.println(intro_question);
-        Scanner sc = new Scanner(System.in);
-        int func_answer = sc.nextInt();
-        switch (func_answer) {
-            case 1:
-                System.out.println("You have chosen: Deposit.");
-                break;
-            case 2:
-                System.out.println("You have chosen: Withdraw");
-                break;
-            default:
-                break;
+        int func_answer;
+        try {
+            func_answer = sc.nextInt();
+        } catch (InputMismatchException e ) {
+            System.out.println("Improper Response. Please try again.");
+            sc.nextLine();
+            introSequence();
+            return;
         }
+        responseConfirmationIntro(func_answer);
+    }
+
+    public static void responseConfirmationIntro(int response) {
+        System.out.println("Arrived here!");
+    }
+    public static void main(String[] args) {
+        sc = new Scanner(System.in);
+        System.out.println("Welcome to an ATM machine!");
+        introSequence();
+        sc.close();
     }
 }
